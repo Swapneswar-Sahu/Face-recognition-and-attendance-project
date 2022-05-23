5703b0aea14d7b239cfdd17f1144389806db55df
swapneswarsahu150@gmail.com <swapneswarsahu150@gmail.com>
2022-05-23 11:45:15 +0530

    added date and enter to close

--- basic.py
+++ basic.py
@@ -35,9 +35,10 @@ def markAttendance(name):
             entry = line.split(',')
             nameList.append(entry[0])
         if name not in nameList:
-            now = datetime.now()
-            dtString = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dtString}')
+           time_now = datetime.now()
+           tString = time_now.strftime('%H:%M:%S')
+           dString = time_now.strftime('%D/%m/%y')
+           f.writelines(f'\n{name},{tString}')
  
 #### FOR CAPTURING SCREEN RATHER THAN WEBCAM
 # def captureScreen(bbox=(300,300,690+300,530+300)):
@@ -70,10 +71,13 @@ while True:
             #print(name)
             y1,x2,y2,x1 = faceLoc
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,255),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,0,255),cv2.FILLED)
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
  
     cv2.imshow('Webcam',img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) == 13:
+     break
+cap.release()
+cv2.destroyAllWindows
